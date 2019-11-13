055a8711cb5ac7136a9137d0c629a7f0ac1dd780
Kevin Stowe <kevincstowe@gmail.com>
2019-11-13 16:38:16 +0100

    working on additional functionality for graph: adding nodes

--- AMR.py
+++ AMR.py
@@ -1,5 +1,3 @@
-import os
-
 TAB_CHAR = "      "
 
 '''
@@ -7,32 +5,20 @@ Load all the graphs in a particular AMR text file into graph objects
 Returns the graph list, along with original strings for comparison. Indexes are aligned
 '''
 def graphs_from_file(input_file):
-    graphs, strings = [], []
+    graphs = []
     cur_gr_str = ""
-    c = 0
-    for line in open(input_file):
-        if not line.split():
-            if (cur_gr_str):
-                graph = graph_from_string(cur_gr_str)
-                graph.file_id = file_id
-                graph.tok = tokens
-                graph.alignments = alignments
-                graphs.append(graph)
-                strings.append(cur_gr_str)
-            cur_gr_str = ""
-            c += 1
-            
-        elif line[0] != "#":
-            cur_gr_str += line
-        else:
-            if "::id" in line:
-                file_id = line.split()[2]
-            if "::tok" in line:
-                tokens = line.split()[2:]
-            if "::alignments" in line:
-                alignments = line.split()[2:]
+    
+    with open(input_file) as input_data:
+        for line in input_data:
+            if not line.split():
+                if (cur_gr_str):
+                    graph = graph_from_string(cur_gr_str)
+                    graphs.append(graph)
+                cur_gr_str = ""
+            elif not line.strip().startswith("#") or "::id" in line or "::tok" in line or "::alignments" in line:
+                cur_gr_str += line
                 
-    return graphs, strings
+    return graphs
 
 def graph_from_string(parse_string):
     node_stack = []
@@ -44,45 +30,53 @@ def graph_from_string(parse_string):
     for line in parse_string.split("\n"):
         if not line.strip():
             continue
-        line_data = line.split()
-        i = 0
-        while i < len(line_data):
-            item = line_data[i]
-            if item[0] == "(":
-                new_node = Node(item[1:], parent_ids = [], children_ids = [], attributes={})
-                nodes[new_node.node_id] = new_node
-                node_stack.append(new_node)
+        
+        if "::id" in line:
+            file_id = line.split()[2]
+        elif "::tok" in line:
+            tokens = line.split()[2:]
+        elif "::alignments" in line:
+            alignments = line.split()[2:]
+        else:
+            line_data = line.split()
+            i = 0
+            while i < len(line_data):
+                item = line_data[i]
+                if item[0] == "(":
+                    new_node = Node(item[1:], parent_ids = [], children_ids = [], attributes={})
+                    nodes[new_node.node_id] = new_node
+                    node_stack.append(new_node)
                 
-                if cur_node:
-                    cur_node.children_ids.append(new_node.node_id)
-                    new_node.parent_ids.append(cur_node.node_id)
-                cur_node = new_node
-            elif item == "/":
-                cur_node.attributes["name"] = line_data[i+1].strip(")")
-            elif item[0] == ":":
-                if line_data[i+1][0] == "(":
-                    cur_edge = Edge(cur_node.node_id, line_data[i+1][1:].strip(")"), item)
-                    edges.append(cur_edge)
-                elif line_data[i+1].strip(")") in nodes or ("~" in line_data[i+1] and line_data[i+1].split("~")[0] in nodes):
-                    cur_edge = Edge(cur_node.node_id, line_data[i+1].strip(")"), item, new_node=False)
-                    edges.append(cur_edge)                
-                elif line_data[i+1][0] == '"':
-                    quote_count = line_data[i+1].count('"')
-                    word = line_data[i+1].rstrip(")")
-                    j = 1
-                    while quote_count < 2:
-                        word += " " + line_data[i+1+j].rstrip(")")
-                        quote_count += line_data[i+1+j].count('"')
-                        j += 1
-                    cur_node.attributes[item] = word
-            i += 1
-            while (item.endswith(")")):
-                item = item[:-1]
-                node_stack.pop()
-                if len(node_stack) > 0:
-                    cur_node = node_stack[-1]
+                    if cur_node:
+                        cur_node.children_ids.append(new_node.node_id)
+                        new_node.parent_ids.append(cur_node.node_id)
+                    cur_node = new_node
+                elif item == "/":
+                    cur_node.attributes["name"] = line_data[i+1].strip(")")
+                elif item[0] == ":":
+                    if line_data[i+1][0] == "(":
+                        cur_edge = Edge(cur_node.node_id, line_data[i+1][1:].strip(")"), item)
+                        edges.append(cur_edge)
+                    elif line_data[i+1].strip(")") in nodes or ("~" in line_data[i+1] and line_data[i+1].split("~")[0] in nodes):
+                        cur_edge = Edge(cur_node.node_id, line_data[i+1].strip(")"), item, new_node=False)
+                        edges.append(cur_edge)                
+                    elif line_data[i+1][0] == '"':
+                        quote_count = line_data[i+1].count('"')
+                        word = line_data[i+1].rstrip(")")
+                        j = 1
+                        while quote_count < 2:
+                            word += " " + line_data[i+1+j].rstrip(")")
+                            quote_count += line_data[i+1+j].count('"')
+                            j += 1
+                        cur_node.attributes[item] = word
+                i += 1
+                while (item.endswith(")")):
+                    item = item[:-1]
+                    node_stack.pop()
+                    if len(node_stack) > 0:
+                        cur_node = node_stack[-1]
                     
-    return Graph(nodes, edges)
+    return Graph(nodes, edges, file_id, tokens, alignments)
 
 def string_from_graph(graph):
     res = []
@@ -142,20 +136,40 @@ def string_from_graph(graph):
 
 
 class Graph(object):
-    def __init__(self, nodes, edges):
+    def __init__(self, nodes, edges, file_id=None, tok=None, alignments=None):
         self.edges = edges
         self.node_dict = nodes
         self.root = None
         for node in nodes.values():
             if len(node.parent_ids) == 0:
                 self.root = node
-        self.file_id = None
-        self.tok = None
-        self.alignments = None
+        self.file_id = file_id
+        self.tok = tok
+        self.alignments = alignments
         
     def __str__(self):
         return "File ID: " + self.file_id + "\nTokens: " + str(self.tok) + "\nAlignments: " + str(self.alignments) + "\nNodes: " + str(self.node_dict) + "\nEdges: " + str(self.edges)
-        
+
+
+    def add_node(self, node, node_id=None, attributes=None, parent_edges=None, children_edges=None):
+        if node_id not in self.node_dict:
+            #TODO need to check that the edge types aren't being duplicated (two ARG0's, zb.)
+            self.node_dict[node_id] = node
+            for edge in parent_edges:
+                self.edges.append(edge)
+                if edge.parent_id not in node.parent_ids:
+                    node.parent_ids.append(edge.parent_id)
+            for edge in children_edges:
+                self.edges.append(edge)
+                if edge.parent_id not in node.children_ids:
+                    node.children_ids.append(edge.parent_id)
+            return True
+        else:
+            #TODO add support for reindexing a new node to add it to graph
+            print ("node id already in graph... cannot currently add")
+                                    
+        return False
+            
 class Node(object):
     def __init__(self, node_id, parent_ids=[], attributes={}, children_ids=[]):
         self.node_id = node_id
--- test_amr.py
+++ test_amr.py
@@ -1,45 +1,55 @@
 import unittest
 import AMR
+import os
 
-input_file = "/home/stowe/Resources/amrs/abstract_meaning_representation_amr_2.0/data/alignments/split/training/amr-release-2.0-alignments-training-bolt.txt"
+DEFAULT_INPUT = "/home/stowe/Resources/amrs/abstract_meaning_representation_amr_2.0/data/alignments/split/training/amr-release-2.0-alignments-training-bolt.txt"
 
 class TestAMR(unittest.TestCase):
+    
     # writes output files to an AMR directory: these are then used as input for the dual graph AMR preprocessor
+    @unittest.skip("only for outputting dualgraph preprocessing input")
     def test_writer(self):
         default_training_input = "/home/stowe/Resources/amrs/abstract_meaning_representation_amr_2.0/data/alignments/split/training/"
         default_dev_input = "/home/stowe/Resources/amrs/abstract_meaning_representation_amr_2.0/data/alignments/split/dev/"
         default_test_input = "/home/stowe/Resources/amrs/abstract_meaning_representation_amr_2.0/data/alignments/split/test/"
 
         with open("/home/stowe/Resources/amrs/NewAmrs/test/data/alignments/split/training/training.txt", "w") as output_file:
-            for input_file in os.listdir(inputs[0]):
-                graphs, strings = graphs_from_file(inputs[0] + input_file)
-                edge_types = set()
-
+            for input_file in os.listdir(default_training_input):
+                graphs = AMR.graphs_from_file(default_training_input + input_file)
+                print (len(graphs))
                 for graph in graphs:
-                    s2 = string_from_graph(graph).strip()
+                    s2 = AMR.string_from_graph(graph).strip()
                     output_file.write(s2+"\n\n")
 
         with open("/home/stowe/Resources/amrs/NewAmrs/test/data/alignments/split/dev/dev.txt", "w") as output_file:
-            for input_file in os.listdir(inputs[1]):
-                graphs, strings = graphs_from_file(inputs[1] + input_file)
-                edge_types = set()
+            for input_file in os.listdir(default_dev_input):
+                graphs = AMR.graphs_from_file(default_dev_input + input_file)
 
                 for graph in graphs:
-                    s2 = string_from_graph(graph).strip()
+                    s2 = AMR.string_from_graph(graph).strip()
                     output_file.write(s2+"\n\n")
 
         with open("/home/stowe/Resources/amrs/NewAmrs/test/data/alignments/split/test/test.txt", "w") as output_file:
-            for input_file in os.listdir(inputs[2]):
-                graphs, strings = graphs_from_file(inputs[2] + input_file)
-                edge_types = set()
+            for input_file in os.listdir(default_test_input):
+                graphs = AMR.graphs_from_file(default_test_input + input_file)
 
                 for graph in graphs:
-                    s2 = string_from_graph(graph).strip()
+                    s2 = AMR.string_from_graph(graph).strip()
                     output_file.write(s2+"\n\n")
 
-        
+    @unittest.skip("basic")
     def test_loader(self):
-        graphs = AMR.graphs_from_file(input_file)
-            
+        graphs = AMR.graphs_from_file(DEFAULT_INPUT)
+        return
+
+    def test_nodes(self):
+        graphs = AMR.graphs_from_file(DEFAULT_INPUT)
+        first_graph = graphs[0]
+        print (first_graph)
+        new_node = AMR.Node("met", parent_ids=[], attributes={"test":"testing_node"}, children_ids=[])
+        first_graph.add_node(new_node)
+        print (first_graph)
+        return
+        
 if __name__ == "__main__":
     unittest.main()
